a870e8ef1b0a6a037154537413864e3e6cc7cd31
Diego <diegoteixeirapereira@hotmail.com>
2021-07-27 16:51:24 -0300

    alteração nos arquivos controle para componente estoque.

--- app/Http/Controllers/Admin/EstoqueController.php
+++ app/Http/Controllers/Admin/EstoqueController.php
@@ -5,11 +5,11 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class AdminController extends Controller
+class EstoqueController extends Controller
 {
     //
-    public function index(){
-       
-        return view('admin.home.index');
+    public function index() {
+
+        return view('admin.estoque.register');
     }
 }
