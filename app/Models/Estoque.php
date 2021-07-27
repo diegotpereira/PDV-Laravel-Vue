2334ea317a8f1d140409654f2ae4d71fd35e6b2b
Diego <diegoteixeirapereira@hotmail.com>
2021-07-27 16:49:47 -0300

    criação dos componentes modelo para estoque.

---
+++ app/Models/Estoque.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\QueryException;
+use App\Models\Estoque\Estoque_aux;
+
+class Estoque extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+    public $maps = ['estoque' => 'number'];
+
+    public function auX() {
+
+        return $this->hasMany('App\Models\Estoque\Estoque_aux', 'codigo_estoque', 'codigo');
+    }
+
+    public static function getOff($codigo, $quantidade) {
+        $estoque = Estoque::where(['codigo', '=', $codigo])->first();
+        $estoque->estoque -= $quantidade;
+
+        try {
+            //code...
+            $estoque->save();
+
+            return true;
+        } catch (QueryException $e) {
+            
+            return false;
+        }
+    }
+
+    public static function getAdd($codigo, $quantidade) {
+        $estoque = Estoque::where(['codigo', '=', $codigo])->first();
+        $estoque->estoque += $quantidade;
+
+        try {
+            //code...
+            $estoque->save();
+        } catch (QueryException $e) {
+            
+            return false;
+        }
+    }
+
+    public static function Total() {
+
+        return Estoque_aux::all()->sum('estoque');
+    }
+
+    public static function valorTotalRS() {
+        $total = 0;
+
+        foreach (Estoque::all() as $estoque) {
+            # code...
+            $total = $total + ($estoque->estoque * $estoque->preco);
+        }
+
+        return number_format($total,2,',','.');
+    }
+
+    public function toRS() {
+
+        return number_format($this->preco,2,',','.');
+    }
+}
---
+++ app/Models/Estoque/Tecido.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Estoque;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tecido extends Model
+{
+    use HasFactory;
+}
