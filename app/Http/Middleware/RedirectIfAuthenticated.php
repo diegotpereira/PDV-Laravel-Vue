8d80e05b34cb93f8522a2c7a7f89e90e75961ce4
Diego <diegoteixeirapereira@hotmail.com>
2021-07-27 16:51:43 -0300

    alteração nos arquivos de configuração do projeto.

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,14 +17,22 @@ class RedirectIfAuthenticated
      * @param  string|null  ...$guards
      * @return mixed
      */
-    public function handle(Request $request, Closure $next, ...$guards)
-    {
-        $guards = empty($guards) ? [null] : $guards;
+    // public function handle(Request $request, Closure $next, ...$guards)
+    // {
+    //     $guards = empty($guards) ? [null] : $guards;
 
-        foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
-            }
+    //     foreach ($guards as $guard) {
+    //         if (Auth::guard($guard)->check()) {
+    //             return redirect(RouteServiceProvider::HOME);
+    //         }
+    //     }
+
+    //     return $next($request);
+    // }
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if (Auth::guard($guard)->check()) {
+            return redirect('/');
         }
 
         return $next($request);
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,31 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/', 'AdminController@index')->name('home');
+Route::group(['middleware' => ['auth'], 'namespace' => 'Admin'], function () {
+    Route::get('/', 'AdminController@index')->name('home');
+    Route::get('/clientes/cadastrar', 'ClientesController@index')->name('clientes.cadastrar');
+    Route::post('/clientes/save', 'ClientesController@cadastrar')->name('clientes.save');
+    Route::get('/clientes/todos', 'ClientesController@lista')->name('clientes.todos');
+    Route::post('/cliente/editar', 'ClientesController@editar')->name('clientes.editar');
+    Route::post('/cliente/saveeditar', 'ClientesController@saveEditar')->name('clientes.saveEdit');
+
+    // Parametros Estoque
+    Route::get('/estoque/cadastrar', 'EstoqueController@index')->name('estoque.cadastrar');
+    Route::post('/estoque/save', 'EstoqueController@cadastrar')->name('estoque.save');
+
+    
+    // Parametros Atributos
+    Route::get('/estoque/modal', 'EstoqueController@viewModal')->name('estoque.atributos.modal');
+    Route::post('/estoque/somente-add', 'EstoqueController@addAtributo')->name('estoque.atributos.add');
+    Route::get('/estoque/somente/{id}', 'EstoqueController@viewAtributos')->name('estoque.atributos');
+    Route::post('ap/estoque/find', 'EstoqueController@APIprocurarEstoqueID')->name('estoque.api.estoqueID');
+
+    // API ESTOQUE
+    Route::get('ap/estoque/{id}', 'EstoqueController@APIFind')->name('estoque.api.find');
+
+});
+Route::get('/mailable', function () { });
+
+Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
