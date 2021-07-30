1f6db2f6e174212bde5c8c2aea8ad4e906d550ca
Diego <diegoteixeirapereira@hotmail.com>
2021-07-30 17:34:43 -0300

    alteração no arquivo controle venda.

--- app/Http/Controllers/Admin/VendasController.php
+++ app/Http/Controllers/Admin/VendasController.php
@@ -12,4 +12,15 @@ class VendasController extends Controller
     public function vendasView() {
         return view('admin.vendas.index', ['aberto'=>Caixa::checkOpen()]);
     }
+
+    public function Registrar(Request $request) {
+        try {
+            //code...
+            $items = array();
+            $total =  count($request->venda['codigo']);
+            $
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+    }
 }
