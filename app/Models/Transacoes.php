710b97766b6adabea70c89e78908592b28809379
Diego <diegoteixeirapereira@hotmail.com>
2021-07-30 13:45:15 -0300

    alteração no arquivo transações adicionado função mouth.

--- app/Models/Transacoes.php
+++ app/Models/Transacoes.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\QueryException;
 
 class Transacoes extends Model
 {
     use HasFactory;
+
+    public static function month(){
+        try {
+            $transacoes = Transacoes::whereMonth('data','=', date('m'))->get();
+            if($transacoes != null){
+                return $transacoes;
+            }else return false;
+        }catch(QueryException $e){
+            return false;
+        }
+    }
 }
