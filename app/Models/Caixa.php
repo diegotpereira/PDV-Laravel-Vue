f5fd4897aa698b569fe0cfaf7745d9591d97bd66
Diego <diegoteixeirapereira@hotmail.com>
2021-07-30 16:30:39 -0300

    alteraçãono arquivo modelo caixa adicionado função checkOpen.

--- app/Models/Caixa.php
+++ app/Models/Caixa.php
@@ -4,8 +4,40 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\QueryException;
 
 class Caixa extends Model
 {
     use HasFactory;
+    public static function checkOpen() {
+        if (Caixa::today() != false && Sistema::getVal('caixa_aberto')) {
+            # code...
+
+            return true;
+        } else {
+            # code...
+
+            return false;
+        }
+        
+    }
+
+    public static function today() {
+        try {
+            //code...
+            $caixa = Caixa::where('data', '=', date('Y-m-d'))->first();
+            if ($caixa != null) {
+                # code...
+                return $caixa;
+            } else {
+                # code...
+                return false;
+            }
+            
+        } catch (QueryException $e) {
+            //throw $th;
+
+            return false;
+        }
+    }
 }
