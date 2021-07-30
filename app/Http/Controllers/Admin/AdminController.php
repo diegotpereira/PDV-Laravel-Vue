1e3287413ba24f8cd7d2563aef85e5ce4da6be5b
Diego <diegoteixeirapereira@hotmail.com>
2021-07-30 13:44:48 -0300

    alteração no arquivo controle index adicionado menu.

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,12 +4,19 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Cliente;
+use App\Models\Estoque;
+use App\Models\Transacoes;
+use App\Models\Estoque\Estoque_aux;
 
 class AdminController extends Controller
 {
     //
     public function index(){
+        $cliente = Cliente::all();
+        $transacoes = count(Transacoes::month());
+        $Estoque = Estoque_aux::Total();
        
-        return view('admin.home.index');
+        return view('admin.home.index',['clientes'=>$cliente,'Estoque'=>$Estoque,'transacoes'=>$transacoes]);
     }
 }
