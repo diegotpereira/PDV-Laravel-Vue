2d6707669769c774355652eb1037d6492847eb21
Diego <diegoteixeirapereira@hotmail.com>
2021-08-03 16:46:51 -0300

    alteração nos arquivos controle para exibição de historico.

--- app/Http/Controllers/Admin/VendasController.php
+++ app/Http/Controllers/Admin/VendasController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Caixa;
+use App\Models\Cliente;
 use App\Models\Entrada_caixa;
 use App\Models\Estoque;
 use App\Models\Estoque\Estoque_aux;
@@ -20,10 +21,18 @@ class VendasController extends Controller
         return view('admin.vendas.index', ['aberto'=>Caixa::checkOpen()]);
     }
 
+    public function moneyParse($val) {
+        return str_replace([".",","],["","."],$val);
+    }
+
+    public function percentParse($val) {
+        return str_replace("%","",$val);
+    }
+
     public function Registrar(Request $request) {
         try {
             //code...
-            $items = array();
+            $itens = array();
             $total = count($request->venda['codigo']);
             $transacao = new Transacoes();
             $transacao->cliente = $request->cliente;
@@ -33,13 +42,13 @@ class VendasController extends Controller
             $transacao->valor_parcelas = $this->moneyParse($request->valor_parcelas);
             $transacao->total = $this->moneyParse($request->total);
 
-            if ($this->percenParse($request->desconto) == '') {
+            if ($this->percentParse($request->desconto) == '') {
                 # code...
                 $transacao->desconto = 0;
 
             } else {
                 # code...
-                $transacao->desconto = $this->perseParse($request->desconto);
+                $transacao->desconto = $this->percentParse($request->desconto);
             }
 
             $transacao->save();
@@ -48,19 +57,24 @@ class VendasController extends Controller
                 # code...
                 $entrada = new Entrada_caixa();
                 $entrada->valor = str_replace([".",","],["","."], $request->dinheiro);
-                $entrada->descricao = "Referente pgcom dinheiro e cartão NT: ".$transacao->id;
-                $entrada::add($entrada->valor);
+                $entrada->descricao = "Referente pg com dinheiro e cartão NT: ".$transacao->id;
+                $entrada->save();
+                Caixa::add($entrada->valor);
+            }
+
+            if($transacao->pagamento ==  "DI"){
+                Caixa::add($transacao->total);
             }
 
             for($i = 0; $i < $total; $i++) {
-                $items[$i]['transacao'] = $transacao->id;
-                $items[$i]['codigo_estoque'] = $request->venda['codigo'][$i];
-                $items[$i]['codigo_estoque_aux'] = $request->venda['codigo_aux'][$i];
-                $items[$i]['quantidade'] = $request->venda['quantidade'][$i];
-                $items[$i]['valor'] = $this->moneyParse($request->venda['total'][$i]);
+                $itens[$i]['transacao'] = $transacao->id;
+                $itens[$i]['codigo_estoque'] = $request->venda['codigo'][$i];
+                $itens[$i]['codigo_estoque_aux'] = $request->venda['codigo_aux'][$i];
+                $itens[$i]['quantidade'] = $request->venda['quantidade'][$i];
+                $itens[$i]['valor'] = $this->moneyParse($request->venda['total'][$i]);
             }
 
-            foreach ($items as $key => $val) {
+            foreach ($itens as $key => $val) {
                 # code...
                 if ($val['codigo_estoque'] != null && $val['quantidade'] != 0 || null) {
                     # code...
@@ -69,8 +83,9 @@ class VendasController extends Controller
                     $vendas->codigo_estoque = $val['codigo_estoque'];
                     $vendas->codigo_estoque_aux = $val['codigo_estoque_aux'];
                     $vendas->quantidade = $val['quantidade'];
+                    $vendas->valor_venda = $val['valor'];
                     $vendas->save();
-                    Estoque_aux::getOff($vendas->codigo_estoqur_aux, $vendas->quantidade);
+                    Estoque_aux::getOff($vendas->codigo_estoque_aux, $vendas->quantidade);
                 }
             }
 
@@ -86,6 +101,90 @@ class VendasController extends Controller
             return response()->json([
                 'success' => 'false',
                 'message' => 'erro no banco de dados, codigo: '.$error_code[2]
+            ]);
+        }
+    }
+
+    public function TransacoesAPIToday() {
+        return Transacoes::today();
+    }
+
+    public function GerarCupom($id) {
+        $detalhes = "";
+        $transacao = Transacoes::where('id', '=', $id)->first();
+        $transacao->cliente = Cliente::where('CPF', '=', $transacao->cliente)->first();
+        $transacao->pagamento = str_replace(['DI', 'CR', 'DE'], ['Dinheiro', 'Cartão de Crédito', 'Débito'], $transacao->pagamento);
+        $transacao->desconto = $transacao->desconto ."%";
+        $transacao->total = number_format($transacao->total, 2, ',','.');
+        $transacao->data = date('d/m/Y', strtotime($transacao->data));
+        $transacao->emissao = date('d/m/Y h:i A');
+        $vendas = Venda::where('transacao', '=', $id)->get();
+        $itens = array();
+        $transacao->subtotal = 0;
+        foreach($vendas as $key=>$val) {
+            $itens[$key] = (object) array_merge (
+                Estoque::where('codigo', '=', $val->codigo_estoque)->first()->toArray(),
+                Estoque_aux::where('id','=', $val->codigo_estoque_aux)->first()->toArray());
+            $itens[$key] ->preco = ($val->valor_venda/$val->quantidade);
+            $transacao->subtotal += ($itens[$key]->preco * $val->quantidade);
+            $itens[$key]->descricao = $itens[$key]->cor .'-'.$itens[$key]->tamanho;
+            $itens[$key]->total = number_format($itens[$key]->preco * $val->quantidade, 2, ',', '.');
+            $itens[$key]->preco = number_format($itens[$key]->preco, 2,',','.');
+            $itens[$key]->quantidade = $val->quantidade;
+        }
+        $transacao->subtotal = number_format($transacao->subtotal,2,',', '.');
+        $transacao->vendas = $itens;
+        if ($id < 10000) {
+            # code...
+            $transacao->NT = '0000'.$transacao->id;
+        } else
+            # code...
+            $transacao->NT = $transacao->id;
+
+            return view('admin.vendas.cupom', ['transacao'=> $transacao]);
+    }
+
+    public function CancelarVenda(Request $request) {
+        try {
+            //code...
+            $id = $request->id;
+            $transacao = Transacoes::where('id','=',$id)->first();
+            $vendas = Venda::where('transacao', '=', $transacao->id)->get();
+
+            foreach($vendas as $venda) {
+                if ($request->return_estoque) {
+                    # code...
+                    Estoque_aux::getAdd($venda->codigo_estoque_aux, $venda->quantidade);
+                }
+                $venda->delete();
+            }
+
+            if ($request->return_caixa) {
+                # code...
+                if ($transacao->pagamento == 'DI') {
+                    # code...
+                    if ($request->return_fechamento) {
+                        # code...
+                        $request->descricao = "Cancelamento de venda - " . $transacao->data;
+                        $request->valor = number_format($transacao->total, 2, ',', '.');
+                        $caixaController = new CaixaController();
+                        $caixaController->sangriaPost($request);
+                    }else{
+                        Caixa::getOff($transacao->total);
+                    }
+                }
+            }    
+                $transacao->delete();
+                return response()->json([
+                    'success' => "true",
+                    'message' => "Venda cancelada com sucesso"
+                ]);
+        }catch (QueryException  $e){
+            //throw $th;
+            $error_code = $e->errorInfo;
+            return response()->json([
+                'success' => 'false',
+                'message' => 'Algum erro ocorreu: '.$error_code[2]
             ]);
         }
     }
