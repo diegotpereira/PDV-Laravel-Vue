feacd6adaba8305891c098b12f15780e50cd6c27
Diego <diegoteixeirapereira@hotmail.com>
2021-08-06 09:44:33 -0300

    alteração no arquivo master.blade modificação no body class.

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -35,7 +35,7 @@
         <!-- Google Font -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
     </head>
-    <body class="hold-transition @yield('body_class')">
+    <body class="skin-red sidebar-mini">
 
     @yield('body')
 
